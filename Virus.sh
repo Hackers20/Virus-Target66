55d1cac974667c48e12d0860607a747c10a90716
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-09-18 12:15:28 +0700

    Virus-Target66

---
+++ Virus.sh
@@ -0,0 +1,69 @@
+GNU nano 5.2                .bashrc
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+figlet VIRUS
+
+echo $cyan"========================================="
+echo $green"    [ Author : Hacker20 ]"
+echo $green"    [ Github : github.com/hacker20 ]"
+echo $cyan"========================================="
+sleep 1
+echo $blue"Login Akun Facebook [+]"
+echo
+echo -n $green"Ussername : ";read is
+echo -n $green"Password  : ";read is
+#sdcard/php hasi.php
+sleep 1
+echo
+echo
+echo
+echo -n $cyan"No Target : ";read isi
+sleep 3
+echo
+echo $red"Sedang Megirim Virus Besar"
+sleep 4
+echo
+echo $green"Loading...[\]"
+echo
+echo
+sleep 1
+echo $red"10%"
+sleep 1
+echo $red"20%"
+sleep 1
+echo $yellow"30%"
+sleep 1
+echo $yellow"40%"
+sleep 1
+echo $cyan"50%"
+sleep 1
+echo $cyqn"60%"
+sleep 1
+echo $blue"70%"
+sleep 1
+echo $blue"80%"
+sleep 1
+echo $green"90%"
+sleep 1
+echo $green"100%"
+echo
+sleep 1
+echo $green"MENGIRIM VIRUS SUKSES KE NOMOR "$isi;
+echo
+echo
+echo
+figlet BYE
+echo $blue"========================================="
+echo $cyan"Terimakasih Sudah Menggunakan Tools Ini"
+echo $cyan"Ingat Jangan Salahgunakan Tools Ini"
+echo $blue"========================================="
+exit
+fi
